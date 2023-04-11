b158cd2a71605eb9d413f5bac9a2777673fafa66
Luis Urbina <luis@luisurbina.net>
2023-04-11 10:45:55 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { Refine, WelcomePage } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { notificationProvider } from "@refinedev/antd";
+import { 
+  ThemedLayout,
+  notificationProvider,
+  ErrorComponent,
+} from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import routerBindings, {
+  NavigateToResource,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Outlet  } from "react-router-dom";
+import { AntdInferencer } from "@refinedev/inferencer/antd";
 import { ColorModeContextProvider } from "./contexts/color-mode";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ColorModeContextProvider>
           <Refine
@@ -25,9 +30,47 @@ function App() {
               syncWithLocation: true,
               warnWhenUnsavedChanges: true,
             }}
+            resources={[
+              {
+                  name: "blog_posts",
+                  list: "/blog-posts",
+                  show: "/blog-posts/show/:id",
+                  create: "/blog-posts/create",
+                  edit: "/blog-posts/edit/:id",
+              },
+          ]}
           >
             <Routes>
-              <Route index element={<WelcomePage />} />
+              <Route
+                  element={
+                      <ThemedLayout>
+                          <Outlet />
+                      </ThemedLayout>
+                  }
+              >
+              <Route
+                  index
+                  element={
+                      <NavigateToResource resource="blog_posts" />
+                  }
+              />
+              <Route path="blog-posts">
+                  <Route index element={<AntdInferencer />} />
+                  <Route
+                      path="show/:id"
+                      element={<AntdInferencer />}
+                  />
+                  <Route
+                      path="edit/:id"
+                      element={<AntdInferencer />}
+                  />
+                  <Route
+                      path="create"
+                      element={<AntdInferencer />}
+                  />
+              </Route>
+              <Route path="*" element={<ErrorComponent />} />
+              </Route>
             </Routes>
             <RefineKbar />
             <UnsavedChangesNotifier />
